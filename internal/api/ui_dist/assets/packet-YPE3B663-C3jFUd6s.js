ca33bbba81c28228e4cbf40fb2a8cca4cc188628
griogrii_riabov <garyabov@mts.ru>
2026-09-05 15:53:21 +0400

    build(ui): regenerate embedded assets after source cleanup

--- internal/api/ui_dist/assets/packet-YPE3B663-C3jFUd6s.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NNHCCRGN-BxqGMN3P.js";import{p as e}from"./mermaid-parser.core-BaPXt7cH.js";export{e as createPacketServices};
---
+++ internal/api/ui_dist/assets/packet-YPE3B663-DIkW-eD2.js
@@ -0,0 +1,1 @@
+import"./chunk-NNHCCRGN-BxqGMN3P.js";import{p as e}from"./mermaid-parser.core-B8Gdy40I.js";export{e as createPacketServices};
