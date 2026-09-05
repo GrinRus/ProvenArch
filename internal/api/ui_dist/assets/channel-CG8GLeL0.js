b372be2fdfbb54fee9da5e0c5a03b31916e13a55
griogrii_riabov <garyabov@mts.ru>
2026-09-05 15:54:55 +0400

    build(ui): regenerate embedded assets after CSS cleanup

--- internal/api/ui_dist/assets/channel-CG8GLeL0.js
+++
@@ -1,1 +0,0 @@
-import{at as e,ot as t}from"./chunk-CSCIHK7Q-B7YYK3ES.js";var n=(n,r)=>t.lang.round(e.parse(n)[r]);export{n as t};
--- internal/api/ui_dist/assets/treemap-LRROVOQU-DlkArkOl.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NNHCCRGN-BxqGMN3P.js";import{a as e}from"./mermaid-parser.core-B8Gdy40I.js";export{e as createTreemapServices};
